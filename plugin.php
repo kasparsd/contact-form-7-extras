e2ddbb3106a9839159bb4b5fbe302c6d971975f0
Kaspars Dambis <hi@kaspars.net>
2017-09-17 21:57:50 +0300

    Bump version

--- plugin.php
+++ plugin.php
@@ -6,8 +6,8 @@
 	Plugin URI: https://github.com/kasparsd/contact-form-7-extras
 	Author: Kaspars Dambis
 	Author URI: https://kaspars.net
-	Version: 0.3.5
-	Tested up to: 4.7.3
+	Version: 0.4.0
+	Tested up to: 4.8.1
 	License: GPL2
 	Text Domain: cf7-extras
 */
