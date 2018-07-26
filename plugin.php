1ef39d4944ed030362f5fd0d52ba0ae4e2d66ff1
Kaspars Dambis <hi@kaspars.net>
2018-07-26 18:00:03 +0300

    Format the docs per standard

--- plugin.php
+++ plugin.php
@@ -1,15 +1,15 @@
 <?php
-/*
-	Plugin Name: Contact Form 7 Controls
-	Description: Add simple controls for the advanced functionality of Contact Form 7.
-	Tags: contact form, contact form 7, cf7, admin, backend, google analytics, ga, forms, form, track
-	Plugin URI: https://github.com/kasparsd/contact-form-7-extras
-	Author: Kaspars Dambis
-	Author URI: https://kaspars.net
-	Version: 0.5.1
-	License: GPL2
-	Text Domain: contact-form-7-extras
-*/
+/**
+ * Plugin Name: Contact Form 7 Controls
+ * Description: Add simple controls for the advanced functionality of Contact Form 7.
+ * Tags: contact form, contact form 7, cf7, admin, backend, google analytics, ga, forms, form, track
+ * Plugin URI: https://github.com/kasparsd/contact-form-7-extras
+ * Author: Kaspars Dambis
+ * Author URI: https://kaspars.net
+ * Version: 0.5.1
+ * License: GPL2
+ * Text Domain: contact-form-7-extras
+ */
 
 include_once dirname( __FILE__ ) . '/src/class-cf7-extras.php';
 
