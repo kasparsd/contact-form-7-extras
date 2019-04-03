b2839445830767da93e95e1cce6de58e1b6ea024
Kaspars Dambis <hi@kaspars.net>
2019-04-03 14:21:06 +0300

    New version

--- plugin.php
+++ plugin.php
@@ -5,7 +5,7 @@
  * Plugin URI: https://formcontrols.com
  * Author: Kaspars Dambis
  * Author URI: https://formcontrols.com
- * Version: 0.7.0
+ * Version: 0.7.1
  * License: GPL2
  * Text Domain: contact-form-7-extras
  */
