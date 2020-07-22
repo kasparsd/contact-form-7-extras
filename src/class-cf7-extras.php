a9f1f1e0d6ac83294b45c53646120f60e45d6400
Kaspars Dambis <hi@kaspars.net>
2020-07-22 19:01:39 +0300

    Bump the asset version

--- src/class-cf7-extras.php
+++ src/class-cf7-extras.php
@@ -12,7 +12,7 @@ class Cf7_Extras {
 	 *
 	 * @var string
 	 */
-	const ASSET_VERSION = '0.6.4';
+	const ASSET_VERSION = '0.6.5';
 
 	/**
 	 * Keep track of forms that have been rendered during the request.
