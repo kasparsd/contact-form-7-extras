673e23d1e272a3e4365d9bc1b87ffc158d293b06
Kaspars Dambis <hi@kaspars.net>
2024-07-11 19:19:56 +0300

    Clarify where to setup

--- plugin.php
+++ plugin.php
@@ -1,7 +1,7 @@
 <?php
 /**
  * Plugin Name: Controls for Contact Form 7
- * Description: Simple controls for the advanced functionality of Contact Form 7. Subscribe to the <a href="https://formcontrols.com/pro" target="_blank">🚀 PRO version</a> for advanced analytics and tracking features.
+ * Description: Use the "Customize" tab in each form settings for advanced controls. Subscribe to the <a href="https://formcontrols.com/pro" target="_blank">🚀 PRO version</a> for advanced analytics and tracking features.
  * Plugin URI: https://formcontrols.com
  * Author: Kaspars Dambis
  * Author URI: https://formcontrols.com
