38d11347a0056b69c69bba32930e5831b40fad43
Kaspars Dambis <hi@kaspars.net>
2024-12-16 09:04:34 +0200

    We’re PHP 5.6 and can do this now

--- plugin.php
+++ plugin.php
@@ -5,13 +5,13 @@
  * Plugin URI: https://formcontrols.com
  * Author: Kaspars Dambis
  * Author URI: https://formcontrols.com
- * Version: 0.8.1
+ * Version: 0.8.2
  * License: GPL2
  * Text Domain: contact-form-7-extras
  */
 
-include_once dirname( __FILE__ ) . '/src/class-cf7-extras.php';
+include_once __DIR__ . '/src/class-cf7-extras.php';
 
 $plugin = Cf7_Extras::instance();
-$plugin->set_plugin_dir( dirname( __FILE__ ) );
+$plugin->set_plugin_dir( __DIR__ );
 $plugin->init();
