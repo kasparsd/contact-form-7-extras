a5c8caee3a8e2073758128eaa20e2384a08908cc
Kaspars Dambis <hi@kaspars.net>
2024-12-16 19:27:52 +0200

    More docs

--- src/class-cf7-extras-form-settings.php
+++ src/class-cf7-extras-form-settings.php
@@ -1,7 +1,15 @@
 <?php
 
+/**
+ * Form settings.
+ */
 class Cf7_Extras_Form_Settings {
 
+	/**
+	 * Default settings.
+	 *
+	 * @var array
+	 */
 	private $defaults = array(
 		'disable-css' => false,
 		'disable-ajax' => false,
@@ -22,18 +30,42 @@ class Cf7_Extras_Form_Settings {
 	 */
 	protected $form;
 
+	/**
+	 * Setup settings for a form.
+	 *
+	 * @param WPCF7_ContactForm $form Form instance.
+	 */
 	public function __construct( $form ) {
 		$this->form = $form;
 	}
 
+	/**
+	 * Get settings for a form by ID.
+	 *
+	 * @param int $form_id Form ID.
+	 *
+	 * @return self
+	 */
 	public static function from_form_id( $form_id ) {
 		return new self( wpcf7_contact_form( $form_id ) );
 	}
 
+	/**
+	 * Get the form ID.
+	 *
+	 * @return int
+	 */
 	public function form_id() {
 		return $this->form->id();
 	}
 
+	/**
+	 * Get a setting value.
+	 *
+	 * @param string $key Setting key.
+	 *
+	 * @return mixed|null Return null if the setting is not found.
+	 */
 	public function get( $key ) {
 		$settings = $this->all();
 
@@ -44,6 +76,11 @@ class Cf7_Extras_Form_Settings {
 		return null;
 	}
 
+	/**
+	 * Get all settings.
+	 *
+	 * @return array
+	 */
 	public function all() {
 		$settings = get_post_meta( $this->form->id(), 'extras', true );
 
--- src/class-cf7-extras-integration.php
+++ src/class-cf7-extras-integration.php
@@ -1,5 +1,8 @@
 <?php
 
+/**
+ * Integration base.
+ */
 abstract class Cf7_Extras_Integration {
 	/**
 	 * HTML input field prefix used in controls settings.
@@ -8,12 +11,29 @@ abstract class Cf7_Extras_Integration {
 	 */
 	const FIELD_PREFIX = 'extra';
 
+	/**
+	 * Initialize the integration.
+	 */
 	abstract public function init();
 
+	/**
+	 * Get the settings instance for a form.
+	 *
+	 * @param int $form_id Form ID.
+	 *
+	 * @return Cf7_Extras_Form_Settings
+	 */
 	public function get_settings( $form_id ) {
 		return Cf7_Extras_Form_Settings::from_form_id( $form_id );
 	}
 
+	/**
+	 * Get the form field name for a field ID.
+	 *
+	 * @param string $field_id Field ID.
+	 *
+	 * @return string
+	 */
 	public function get_field_name( $field_id ) {
 		return sprintf( '%s[%s]', self::FIELD_PREFIX, $field_id );
 	}
