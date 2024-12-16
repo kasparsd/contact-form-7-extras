42a9429ac04fb481f3310190ac7998a34409c62a
Kaspars Dambis <hi@kaspars.net>
2024-12-16 11:28:50 +0200

    Describe the test

--- tests/phpunit/class-plugin-test.php
+++ tests/phpunit/class-plugin-test.php
@@ -1,7 +1,7 @@
 <?php
 
 class Plugin_Test extends WP_UnitTestCase {
-	public function test_plugin() {
-		$this->assertTrue( class_exists( 'Cf7_Extras' ) );
+	public function test_plugin_active() {
+		$this->assertTrue( class_exists( 'Cf7_Extras' ), 'Plugin is active' );
 	}
 }
