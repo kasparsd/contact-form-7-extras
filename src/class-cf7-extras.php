5a2e0384fe680b21ed1d6b7ee7dc08f52831bc8d
Kaspars Dambis <hi@kaspars.net>
2025-01-16 09:41:59 +0200

    The assets have changed

--- src/class-cf7-extras.php
+++ src/class-cf7-extras.php
@@ -12,7 +12,7 @@ class Cf7_Extras {
 	 *
 	 * @var string
 	 */
-	const ASSET_VERSION = '0.8.0';
+	const ASSET_VERSION = '0.9.0';
 
 	/**
 	 * Keep track of forms that have been rendered during the request.
